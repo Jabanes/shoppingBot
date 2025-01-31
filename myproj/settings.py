ccacb3f88b0af02d0794ad3f9b9791ab00051c5c
Your NameJabanes <erezhabani2003@gmail.com>
2025-01-31 12:20:40 +0200

    added debug false as defualt env variable

--- myproj/settings.py
+++ myproj/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
     raise ValueError("The SECRET_KEY environment variable is not set.") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
